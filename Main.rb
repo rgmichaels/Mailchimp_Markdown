73332c1d741524ae6a64aa58097545e84430feb7
Rob Michaels <rgmichaels@gmail.com>
2022-01-03 15:57:03 -0500

    Refactored for speed and readability

--- Main.rb
+++ Main.rb
@@ -1,21 +1,10 @@
+
 #!/usr/bin/env ruby\
 require "rubygems"
 require "uri"
 require "date"
 
 #Methods are above and I call the methods at the end of the file
-
-
-# Needs error checking on file operations
-# Needs more error checking overall
-# Needs protection against output and log file sizes.  Suggest new files each day with date stamp
-# For a test I would create a consistent sample input file and simply check (assert) the contents of that file
-# Ideally I'd likely use the redcarpet gem which handles the real solution
-# Additional tests:
-#   Large file handling, find breakpoint.
-#   Large file handling, memory usage.
-#   If a breakpoint is found, calculate # of lines or size and log the error.
-#   Make sure files are closed properly.
 
 #Write a time stamp to output file
 File.write("Output/markdownoutput.txt", Time.now, mode:"a")
@@ -26,55 +15,42 @@ File.write("Logs/MailchimpMarkdown.log", Time.now.to_s + " Mailchimp Markdown St
 # Open input data file for use and execute code for each line
 File.open("supportingfiles/markupexample.txt").each do | line |
 
-# This parses the headings #, ##, etc
-def multiheadings(line)
+
+  # This parses the headings #, ##, etc
+  def multiheadings(line)
+      mynewline = "<h" + line.split.first.length.to_s + ">" + line.split(' ')[1..-1].join(' ') + "</h" + line.split.first.length.to_s + ">"
+      puts mynewline
+      File.write("Output/markdownoutput.txt", mynewline + "\n", mode:"a")
+  end
+
+  #build anchor tags
+  #This requires testing
+  def anchors (line)
+      linktext = line.split("[").last.split("]").first
+      linkurl = line.split("(").last.split(")").first
+      markdownlink = "<a href=\"" + linkurl + ">" + linktext + "</a>"
+      puts markdownlink
+      File.write("Output/markdownoutput.txt", markdownlink + "\n", mode:"a")
+  end
+
+  # Handles unformatted text lines (p tags).
+  def unformatted(line)
+        unformattedline = "<p>" + line + "</p>"
+        puts unformattedline.gsub("\n","")
+        File.write("Output/markdownoutput.txt", unformattedline + "\n", mode:"a")
+        # Spaces, nulls need to be tested/handled
+  end
+
+  # This executes the methods
   if line[0,1] == "#"
-    mynewline = "<h" + line.split.first.length.to_s + ">" + line.split(' ')[1..-1].join(' ') + "</h" + line.split.first.length.to_s + ">"
-    puts mynewline
-    File.write("Output/markdownoutput.txt", mynewline + "\n", mode:"a")
-    end
+    multiheadings line
   end
-
-#build anchor tags
-  # inline anchors need to be handled, might need some regex work or an acceptable gem/library
-  # multiple anchors may not be handled
-def anchors(line)
-  if line.include? "http" #Link
-  linktext = line.split("[").last.split("]").first
-  line = line.gsub!(linktext,"<a href=\"") + linktext + "</a>"
-  line = line.gsub!("\n","") # a new line kept appearing after the link text!
-  newline = line.gsub!("]","")
-  newline = newline.gsub!("(","")
-  newline = newline.gsub!(")","\">")
-  puts newline.gsub("\n","")[1..-1]
-  File.write("Output/markdownoutput.txt", newline.gsub("\n","")[1..-1] + "\n", mode:"a")
-                    end
-end
-
-# Handles unformatted text lines (p tags)
-# Refactoring could do some justice in the ifs.
-def unformatted(line)
+  if line.include?("http")
+    anchors line
+  end
   if line[0,1] != "#" and line[0,1] != "["
-
-  if line[0,1] != "#" or unless line.include?("http") or unless line.include?("https")
-                                                           #other link types need to be handled like ftp
-                                                         end
-                         end
-    mynewline = "<p>" + line + "</p>"
-    puts mynewline.gsub("\n","")
-    File.write("Output/markdownoutput.txt", mynewline + "\n", mode:"a")
-    # had some inconsistencies between screen output and file output thus the \n work
-    # Spaces, nulls need to be handled
+    unformatted line
   end
-  end
-
-end
-
-# this should be faster if IFs were moved here instead of running unnecessary code
-  multiheadings line
-  anchors line
-  unformatted line
-
 end # ends the initial for loop for file opening
 
 #Write completion to log
--- Readme.txt
+++ Readme.txt
@@ -1,8 +1,7 @@
 Hello there and happy new year!
 
-This is certainly far from ideal, especially since there are
-proven gems that accomplish all of this in a few lines.  (redcarpet?)
-There are edge cases that would need to be handled.
+I'd probably use the redcarpet gem for a real solution to this, it handles markdown -> html.
+There are edge cases that would need to be handled, especially in the anchor tags.
 
 This was made with Ruby 2.7 (x64) but it's pretty basic and should run on all versions.
 
@@ -13,4 +12,15 @@ It takes an input file (SupportingFiles/markdownexample.txt)
 and outputs to screen and Output/markdownoutput.txt.
 
 A basic error log is written to Logs/MailchimpMarkdown.log.
+
+# Needs error checking on file operations
+# Needs more error checking overall
+# Needs protection against output and log file sizes.  Suggest new files each day with date stamp.
+# For a test I would create a consistent sample input file and simply check (assert) the contents of that file
+# Ideally I'd likely use the redcarpet gem which handles the real solution
+# Additional tests:
+#   Large file handling, find breakpoint.
+#   Large file handling, memory usage.
+#   If a break point is found, calculate # of lines or size and log the error.
+#   Make sure files are closed properly.
 
