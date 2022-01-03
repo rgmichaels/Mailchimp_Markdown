8b15c598989edd76e343c8ccc9e1443ba32e164f
Rob Michaels <rgmichaels@gmail.com>
2022-01-03 08:48:33 -0500

    8:50AM Edits, remove files, etc

--- Main.rb
+++ Main.rb
@@ -1,97 +1,86 @@
 #!/usr/bin/env ruby\
-require  "rubygems"
+require "rubygems"
 require "uri"
 require "date"
 
+#Methods are above and I call the methods at the end of the file
 
+
+# Needs error checking on file operations
+# Needs more error checking overall
+# Needs protection against output and log file sizes.  Suggest new files each day with date stamp
+# For a test I would create a consistent sample input file and simply check (assert) the contents of that file
+# Ideally I'd likely use the redcarpet gem which handles the real solution
+# Additional tests:
+#   Large file handling, find breakpoint.
+#   Large file handling, memory usage.
+#   If a breakpoint is found, calculate # of lines or size and log the error.
+#   Make sure files are closed properly.
 
 #Write a time stamp to output file
-File.write("markdownoutput.txt", Time.now, mode:"a")
+File.write("Output/markdownoutput.txt", Time.now, mode:"a")
 
-# Write start to log
+# Write time and start to log
 File.write("Logs/MailchimpMarkdown.log", Time.now.to_s + " Mailchimp Markdown Started\n", mode:"a")
-# Needs error checking to see if file can be opened
 
-datafile = File.read("supportingfiles/markupexample2.txt")
-# Needs error checking to see if file can be opened
+# Open input data file for use and execute code for each line
+File.open("supportingfiles/markupexample.txt").each do | line |
 
-line_num = 0
-File.open("supportingfiles/markupexample2.txt").each do | line |
-  #puts "#{line_num += 1} #{line}"
-  #puts "#{line}"
-
-
-#  Methods
 # This parses the headings #, ##, etc
-# Detect Headings
 def multiheadings(line)
   if line[0,1] == "#"
-    # creates a new var mynewline as the var that will hold the markdown
-    # adds <h
-    # splits the first word out
-    # counts the length of that first word for use as the header number
-    # converts to string
-    # and builds the rest of the html string
-    # <h + number of chars + >  + original string + </h + number of chars + >
     mynewline = "<h" + line.split.first.length.to_s + ">" + line.split(' ')[1..-1].join(' ') + "</h" + line.split.first.length.to_s + ">"
     puts mynewline
-    File.write("markdownoutput.txt", mynewline, mode:"a")
-    File.write("markdownoutput.txt", "\n", mode:"a")
+    File.write("Output/markdownoutput.txt", mynewline + "\n", mode:"a")
     end
   end
 
-
-
-#deal with anchor tags
+#build anchor tags
+  # inline anchors need to be handled, might need some regex work or an acceptable gem/library
+  # multiple anchors may not be handled
 def anchors(line)
-  if line.include? "Link"
+  if line.include? "http" #Link
   linktext = line.split("[").last.split("]").first
   line = line.gsub!(linktext,"<a href=\"") + linktext + "</a>"
+  line = line.gsub!("\n","") # a new line kept appearing after the link text!
   newline = line.gsub!("]","")
   newline = newline.gsub!("(","")
   newline = newline.gsub!(")","\">")
   puts newline.gsub("\n","")[1..-1]
-  File.write("markdownoutput.txt", newline, mode:"a")
-  File.write("markdownoutput.txt", "\n", mode:"a")
+  File.write("Output/markdownoutput.txt", newline.gsub("\n","")[1..-1] + "\n", mode:"a")
                     end
 end
 
-
-
-# This is unformatted text to wrap in <p> tags
-# See if string length is > 0 then build the new string wrapped in tags
-# This should be the fallback code
-
+# Handles unformatted text lines (p tags)
+# Refactoring could do some justice in the ifs.
 def unformatted(line)
+  if line[0,1] != "#" and line[0,1] != "["
+
   if line[0,1] != "#" or unless line.include?("http") or unless line.include?("https")
-                                                           #ftp?
-                                                           end
-  #if line.length > 0 and line[0,1] != "#"
+                                                           #other link types need to be handled like ftp
+                                                         end
+                         end
     mynewline = "<p>" + line + "</p>"
     puts mynewline.gsub("\n","")
-    File.write("markdownoutput.txt", mynewline, mode:"a")
-    File.write("markdownoutput.txt", "\n", mode:"a")
-    # had to use that gsub because a newline kept going in places!
-    # What if just a space?  Or Null?
+    File.write("Output/markdownoutput.txt", mynewline + "\n", mode:"a")
+    # had some inconsistencies between screen output and file output thus the \n work
+    # Spaces, nulls need to be handled
   end
   end
-  end
 
+end
 
-
-
+# this should be faster if IFs were moved here instead of running unnecessary code
   multiheadings line
   anchors line
   unformatted line
 
-end
+end # ends the initial for loop for file opening
 
 #Write completion to log
 File.write("Logs/MailchimpMarkdown.log", Time.now.to_s + " Mailchimp Markdown Completed\n", mode:"a")
 
 
-# Needs error checking on file operations
-# Needs protection against output and log file sizes.  Suggest new files each day with date stamp
-#
+
 
 
---
+++ Readme.txt
@@ -0,0 +1,15 @@
+Hello there and happy new year!
+
+This is certainly far from ideal, especially since there are
+proven gems that accomplish all of this in a few lines.  (redcarpet?)
+There are edge cases that would need to be handled.
+
+This was made with Ruby 2.7 (x64) but it's pretty basic and should run on all versions.
+
+It should be able to run via command prompt, however I have been running through Rubymine.
+
+It takes an input file (SupportingFiles/markdownexample.txt)
+and outputs to screen and Output/markdownoutput.txt.
+
+A basic error log is written to Logs/MailchimpMarkdown.log.
+
